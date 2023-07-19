fbedaf86f056738b7042498f649a75448d602347
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-19 13:15:55 +0300

    add validation to input fields

--- js/script.js
+++ js/script.js
@@ -14,12 +14,36 @@ function addExpenseRecord() {
     const name = name_input.val()
     const amount = amount_input.val()
 
-    const expense_record = $(expenseRecord(name,amount))
+    if (validate(name,amount)){
+        const expense_record = $(expenseRecord(name,amount))
 
-    expenses_container.append(expense_record)
-    // console.log(expense_record)
-    name_input.val("")
-    amount_input.val("")
+        expenses_container.append(expense_record)
+        name_input.val("")
+        amount_input.val("")
+    }
+    else{
+        console.log("INVALID")
+    }
+}
+
+function validate(name,amount){
+    if (name.trim() != "" || amount.trim() != ""){
+        const nameregex = /^[A-Za-z\s]*$/;
+        const amountregex = /^[0-9]+$/;
+        
+        if(nameregex.test(name)){
+            if(amountregex.test(amount)){
+                return true
+            }
+            else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+    else
+        return false
 }
 
 $(document).ready(function () {
